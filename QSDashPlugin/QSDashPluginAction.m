0cd7d0e7166a9d26618c5cb374ac16645952f29c
Rob McBroom <github@skurfer.com>
2016-02-07 21:00:51 -0600

    add the DocSet search actions

--- QSDashPlugin/QSDashPluginAction.m
+++ QSDashPlugin/QSDashPluginAction.m
@@ -6,11 +6,40 @@
 //
 
 #import "QSDashPluginAction.h"
+#import "QSDashPlugin.h"
 
 @implementation QSDashPluginActionProvider
 
-- (QSObject *)performActionOnObject:(QSObject *)dObject
+- (QSObject *)searchDocSet:(QSObject *)dObject query:(QSObject *)iObject
 {
+	NSString *dashURLformat = @"dash-plugin://keys=%@&query=%@";
+	NSArray *docsetObjects = [dObject splitObjects];
+	NSString *query = [iObject stringValue];
+	NSArray *platforms = [docsetObjects arrayByEnumeratingArrayUsingBlock:^id(QSObject *ds) {
+		return [ds objectForType:QSDashDocsetType];
+	}];
+	NSString *joined = [platforms componentsJoinedByString:@","];
+	NSString *dashURLstr = [NSString stringWithFormat:dashURLformat, joined, query];
+	NSURL *dashURL = [NSURL URLWithString:dashURLstr];
+	if (dashURL) {
+		[[NSWorkspace sharedWorkspace] openURL:dashURL];
+	} else {
+		NSLog(@"error with location: %@", dashURLstr);
+	}
+	return nil;
+}
+
+- (NSArray *)validIndirectObjectsForAction:(NSString *)action directObject:(QSObject *)dObject
+{
+	// sort DocSets by use frequency in the third pane
+	if ([action isEqualToString:@"QSDashSearchReversed"]) {
+		return [QSLib scoredArrayForType:QSDashDocsetType];
+	}
+	// present a text-entry box
+	else if ([action isEqualToString:@"QSDashSearch"]) {
+		NSString *searchString = [[NSPasteboard pasteboardWithName:NSFindPboard] stringForType:NSStringPboardType];
+		return [NSArray arrayWithObject: [QSObject textProxyObjectWithDefaultValue:(searchString?searchString:@"")]];
+	}
 	return nil;
 }
 @end
